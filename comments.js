e90fb0a7a761e52c9337248d13ae7d3ded7d142e
I.C.D. Samaraweera <chamidilshan55@gmail.com>
2023-07-18 01:32:41 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,35 @@
-//create web server
+//Create web server
 const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+//Load static files
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+//Load template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+//Load routes
+const routes = require('./routes/index');
+app.use('/', routes);
+
+//Error handling
+app.use((req, res, next) => {
+    const err = new Error('Page not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    //res.locals.error = err;
+    res.status(err.status);
+    res.render('error', { err });
+});
+
+//Create server
+app.listen(port, () => {
+    console.log(`The application is running on localhost:${port}!`);
+});
