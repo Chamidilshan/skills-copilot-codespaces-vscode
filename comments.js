ef4473bf4f734f94167a524ef70fddbbc70c3210
I.C.D. Samaraweera <chamidilshan55@gmail.com>
2023-07-18 01:22:49 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,37 @@
 //create web server
-const express = require('express');
-const router = express.Router();
-const { Comment } = require('../models/Comment');
-const { auth } = require('../middleware/auth');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    console.log( data );
+    res.end( data );
+  });
+})
+
+app.get('/addUser', function (req, res) {
+  // First read existing users.
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    data = JSON.parse( data );
+    data["user4"] = user["user4"];
+    console.log( data );
+    res.end( JSON.stringify(data));
+  });
+})
+
+app.get('/:id', function (req, res) {
+  // First read existing users.
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    var users = JSON.parse( data );
+    var user = users["user" + req.params.id]
+    console.log( user );
+    res.end( JSON.stringify(user));
+  });
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+})
